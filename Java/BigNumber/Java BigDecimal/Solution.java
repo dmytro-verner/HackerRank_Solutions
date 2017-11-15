2eb9c5b24c9b40abeda083341c519d9c1e54cc23
Dmitryi <dmytro.verner@gmail.com>
2017-11-15 15:54:09 +0200

    Added solution to BigDecimal task

---
+++ Java/BigNumber/Java BigDecimal/Solution.java
@@ -0,0 +1,23 @@
+import java.math.BigDecimal;
+import java.util.*;
+
+class Solution{
+    public static void main(String []args){
+        //Input
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        String []s=new String[n+2];
+        for(int i=0;i<n;i++){
+            s[i]=sc.next();
+        }
+        sc.close();
+		
+		Arrays.sort(s, 0, n, Collections.reverseOrder((s1, s2) -> new BigDecimal(s1).compareTo(new BigDecimal(s2))));
+		
+		//Output
+        for(int i=0;i<n;i++)
+        {
+            System.out.println(s[i]);
+        }
+    }
+}
