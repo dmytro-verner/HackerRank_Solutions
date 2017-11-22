4bfd9b32f607abaa40c27124d96baee8bb5b1a3a
Dmytro Verner <31669192+dmytro-verner@users.noreply.github.com>
2017-11-22 21:23:54 +0200

    Update Solution.java

--- Java/Data Structures/Java Arraylist/Solution.java
+++ Java/Data Structures/Java Arraylist/Solution.java
@@ -7,7 +7,7 @@ import java.util.regex.*;
 public class Solution {
 
     public static void main(String[] args) {
-          Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         int listOfListsSize = scanner.nextInt();
 
         List<List<Integer>> listOfLists = new ArrayList<>(listOfListsSize);
