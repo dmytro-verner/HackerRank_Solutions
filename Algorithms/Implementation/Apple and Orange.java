bb891f1880d2016a49e9c119b09c2d30fa47a36f
Dmitryi <dmytro.verner@gmail.com>
2018-04-15 00:09:21 +0300

    Added two Implementation algorithms solutions

---
+++ Algorithms/Implementation/Apple and Orange.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges) {
+ 
+        long applesAtHouseCount = Arrays.stream(apples)
+                                        .filter(x -> x + a >= s && x + a <= t)
+                                        .count();
+        long orangesAtHouseCount = Arrays.stream(oranges)
+                                        .filter(x -> x + b >= s && x + b <= t)
+                                        .count();
+        
+        System.out.println(applesAtHouseCount);
+        System.out.println(orangesAtHouseCount);
+    }
+
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String[] st = scan.nextLine().split(" ");
+
+        int s = Integer.parseInt(st[0].trim());
+
+        int t = Integer.parseInt(st[1].trim());
+
+        String[] ab = scan.nextLine().split(" ");
+
+        int a = Integer.parseInt(ab[0].trim());
+
+        int b = Integer.parseInt(ab[1].trim());
+
+        String[] mn = scan.nextLine().split(" ");
+
+        int m = Integer.parseInt(mn[0].trim());
+
+        int n = Integer.parseInt(mn[1].trim());
+
+        int[] apple = new int[m];
+
+        String[] appleItems = scan.nextLine().split(" ");
+
+        for (int appleItr = 0; appleItr < m; appleItr++) {
+            int appleItem = Integer.parseInt(appleItems[appleItr].trim());
+            apple[appleItr] = appleItem;
+        }
+
+        int[] orange = new int[n];
+
+        String[] orangeItems = scan.nextLine().split(" ");
+
+        for (int orangeItr = 0; orangeItr < n; orangeItr++) {
+            int orangeItem = Integer.parseInt(orangeItems[orangeItr].trim());
+            orange[orangeItr] = orangeItem;
+        }
+
+        countApplesAndOranges(s, t, a, b, apple, orange);
+    }
+}
