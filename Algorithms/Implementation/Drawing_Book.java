7596eabd9f7f81c402744d4d02b6e9b2026f8e1f
Dmytro Verner <dmytro.verner@gmail.com>
2019-05-29 23:01:21 +0300

    [Implementation] Drawing Book solution

---
+++ Algorithms/Implementation/Drawing_Book.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int pageCount(int totalPages, int targetPage) {
+        return Math.min(targetPage / 2, totalPages / 2 - targetPage / 2);
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])*");
+
+        int p = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])*");
+
+        int result = pageCount(n, p);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
