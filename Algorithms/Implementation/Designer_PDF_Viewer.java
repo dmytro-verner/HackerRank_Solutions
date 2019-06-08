f10da367a099efbb5450cb0d16d5979b50397358
Dmytro Verner <dmytro.verner@gmail.com>
2019-06-09 02:38:32 +0300

    [Implementation] Designer PDF Viewer solution

---
+++ Algorithms/Implementation/Designer_PDF_Viewer.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+import java.util.stream.*;
+
+public class Solution {
+
+    O(n) - time complexity
+    static int designerPdfViewer(int[] h, String word) {
+        int maxHeight = word.chars().map(c -> h[c - (int)'a']).max().getAsInt();
+        return maxHeight * word.length();
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int[] h = new int[26];
+
+        String[] hItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < 26; i++) {
+            int hItem = Integer.parseInt(hItems[i]);
+            h[i] = hItem;
+        }
+
+        String word = scanner.nextLine();
+
+        int result = designerPdfViewer(h, word);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
